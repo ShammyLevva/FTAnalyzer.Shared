1ccfbf4aa950d6c94aee3d3157101f11eb2d0838
Deryck Brown <deryckfb@sky.com>
2018-09-30 21:01:05 +0100

    Tidied up CensusFamily.

--- FTAnalyzer.Shared/Core/CensusFamily.cs
+++ FTAnalyzer.Shared/Core/CensusFamily.cs
@@ -11,27 +11,35 @@ namespace FTAnalyzer
         public new CensusIndividual Husband { get; private set; }
         public new CensusIndividual Wife { get; private set; }
         public new List<CensusIndividual> Children { get; private set; }
-        private List<CensusIndividual> FamilyChildren { get; set; }
-        private Family BaseFamily { get; set; }
+
+        List<CensusIndividual> FamilyChildren { get; set; }
+        Family BaseFamily { get; set; }
 
         public CensusFamily(Family f, CensusDate censusDate)
             : base(f)
         {
             BaseFamily = f;
-            FamilyTree ft = FamilyTree.Instance;
             CensusDate = censusDate;
             BestLocation = null;
-            int position = 1;
+            var position = 1;
+
             if (f.Wife != null)
-                Wife = new CensusIndividual(position++, f.Wife, this, CensusIndividual.WIFE);
-            if (f.Husband != null)
-                Husband = new CensusIndividual(position++, f.Husband, this, CensusIndividual.HUSBAND);
-            Children = new List<CensusIndividual>();
-            foreach (Individual child in f.Children)
             {
-                CensusIndividual toAdd = new CensusIndividual(position++, child, this, CensusIndividual.CHILD);
+                Wife = new CensusIndividual(position++, f.Wife, this, CensusIndividual.WIFE);
+            }
+
+            if (f.Husband != null)
+            {
+                Husband = new CensusIndividual(position++, f.Husband, this, CensusIndividual.HUSBAND);
+            }
+
+            Children = new List<CensusIndividual>();
+            foreach (var child in f.Children)
+            {
+                var toAdd = new CensusIndividual(position++, child, this, CensusIndividual.CHILD);
                 Children.Add(toAdd);
             }
+
             FamilyChildren = new List<CensusIndividual>(Children); // Family children is all children alive or dead at census date
         }
 
@@ -39,18 +47,20 @@ namespace FTAnalyzer
         {
             get
             {
-                if (Husband != null) yield return Husband;
-                if (Wife != null) yield return Wife;
+                if (Husband != null) { yield return Husband; }
+                if (Wife != null) { yield return Wife; }
                 if (Children != null && Children.Count > 0)
-                    foreach (CensusIndividual child in Children) yield return child;
+                {
+                    foreach (var child in Children) { yield return child; }
+                }
             }
         }
 
         public bool Process(CensusDate censusDate, bool censusDone, bool checkCensus)
         {
-            bool result = false;
+            var result = false;
             CensusDate = censusDate;
-            List<Fact> facts = new List<Fact>();
+            var facts = new List<Fact>();
             if (IsValidFamily())
             {
                 if (IsValidIndividual(Wife, censusDone, true, checkCensus))
@@ -59,7 +69,10 @@ namespace FTAnalyzer
                     facts.AddRange(Wife.PersonalFacts);
                 }
                 else
+                {
                     Wife = null;
+                }
+
                 // overwrite bestLocation by husbands as most commonly the family
                 // end up at husbands location after marriage
                 if (IsValidIndividual(Husband, censusDone, true, checkCensus))
@@ -68,16 +81,22 @@ namespace FTAnalyzer
                     facts.AddRange(Husband.PersonalFacts);
                 }
                 else
+                {
                     Husband = null;
+                }
+
                 // update bestLocation by marriage date as Husband and Wife 
                 // locations are often birth locations
-                Fact marriage = GetPreferredFact(Fact.MARRIAGE);
+                var marriage = GetPreferredFact(Fact.MARRIAGE);
                 if (marriage != null)
+                {
                     facts.Add(marriage);
+                }
+
                 var censusChildren = new List<CensusIndividual>();
                 // sort children oldest first
                 Children.Sort(new CensusAgeComparer());
-                foreach (CensusIndividual child in Children)
+                foreach (var child in Children)
                 {
                     // set location to childs birth location
                     // this will end up setting birth location of last child 
@@ -89,49 +108,57 @@ namespace FTAnalyzer
                         facts.AddRange(child.PersonalFacts);
                     }
                 }
+
                 Children = censusChildren;
                 BestLocation = FactLocation.BestLocation(facts, censusDate);
             }
+
             return result;
         }
 
-        private bool IsValidIndividual(CensusIndividual indiv, bool censusDone, bool parentCheck, bool checkCensus)
+        bool IsValidIndividual(CensusIndividual indiv, bool censusDone, bool parentCheck, bool checkCensus)
         {
             if (indiv == null)
-                return false;
-            DateTime birth = indiv.BirthDate.StartDate;
-            DateTime death = indiv.DeathDate.EndDate;
-            FactLocation bestLocation = indiv.BestLocation(CensusDate);
-            if (birth <= CensusDate.StartDate && death >= CensusDate.StartDate)
             {
-                if ((checkCensus && indiv.IsCensusDone(CensusDate) == censusDone && !indiv.OutOfCountry(CensusDate)) || !checkCensus)
-                {
-                    if (parentCheck) // Husband or Wife with valid date range
-                        return true;
-                    else // individual is a child so remove if married before census date
-                        return !indiv.IsMarried(CensusDate);
-                }
-                else
-                    return false;
-            }
-            else
                 return false;
+            }
+
+            var birth = indiv.BirthDate.StartDate;
+            var death = indiv.DeathDate.EndDate;
+            var bestLocation = indiv.BestLocation(CensusDate);
+
+            return birth <= CensusDate.StartDate && death >= CensusDate.StartDate && (
+                    checkCensus && indiv.IsCensusDone(CensusDate) == censusDone && !indiv.OutOfCountry(CensusDate)) ||
+                    !checkCensus && parentCheck ||
+                    !indiv.IsMarried(CensusDate);
         }
 
-        private bool IsValidFamily()
+        bool IsValidFamily()
         {
-            Individual eldestChild = Children.OrderBy(x => x.BirthDate).FirstOrDefault();
+            var eldestChild = Children.OrderBy(x => x.BirthDate).FirstOrDefault();
             if (MarriageDate.IsAfter(CensusDate) && (eldestChild == null || eldestChild.BirthDate.IsAfter(CensusDate)))
+            {
                 return false;
+            }
+
             if (FamilyType.Equals(Family.SOLOINDIVIDUAL) || FamilyType.Equals(Family.PRE_MARRIAGE))
+            {
                 return true; // allow solo individual families to be processed
+            }
+
             // don't process family if either parent is under 16
             //if(Husband != null) rtb.AppendText("Husband : " + Husband.getAge(censusDate) + "\n");
             if (Husband != null && Husband.GetMaxAge(CensusDate) < 16)
+            {
                 return false;
+            }
+
             //if(Wife  != null) rtb.AppendText("Wife : " + Wife.getAge(censusDate) + "\n");
             if (Wife != null && Wife.GetMaxAge(CensusDate) < 16)
+            {
                 return false;
+            }
+
             return true;
         }
 
@@ -139,50 +166,36 @@ namespace FTAnalyzer
         {
             get
             {
-                if (Husband != null) return Husband.SurnameAtDate(CensusDate);
-                else if (Wife != null) return Wife.SurnameAtDate(CensusDate);
-                else
-                {
-                    CensusIndividual child = Children.FirstOrDefault();
-                    if (child != null)
-                    {
-                        return child.SurnameAtDate(CensusDate);
-                    }
-                }
-                return Individual.UNKNOWN_NAME;
+                if (Husband != null) { return Husband.SurnameAtDate(CensusDate); }
+                if (Wife != null) { return Wife.SurnameAtDate(CensusDate); }
+
+                var child = Children.FirstOrDefault();
+                return child != null ? child.SurnameAtDate(CensusDate) : Individual.UNKNOWN_NAME;
             }
         }
 
-        public int ChildrenAlive
-        {
-            get { return FamilyChildren.Count(x => x.IsAlive(CensusDate)); }
-        }
+        public int ChildrenAlive => FamilyChildren.Count(x => x.IsAlive(CensusDate));
 
-        public int ChildrenDead
-        {
-            get { return FamilyChildren.Count(x => !x.IsAlive(CensusDate) && x.BirthDate.IsBefore(CensusDate)); }
-        }
+        public int ChildrenDead => FamilyChildren.Count(x => !x.IsAlive(CensusDate) && x.BirthDate.IsBefore(CensusDate));
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String)")]
         public int ChildrenTotal
         {
             get
             {
-                int total = ChildrenAlive + ChildrenDead;
+                var total = ChildrenAlive + ChildrenDead;
                 if (total == 0)
+                {
                     Console.WriteLine("hmmm zero?");
+                }
                 return total;
             }
         }
 
-        string IDisplayChildrenStatus.Husband
-        {
-            get { return BaseFamily.Husband == null ? string.Empty : BaseFamily.Husband.Name + " (b." + BaseFamily.Husband.BirthDate + ")"; }
-        }
+        string IDisplayChildrenStatus.Husband =>
+                BaseFamily.Husband == null ? string.Empty : $"{BaseFamily.Husband.Name} (b. {BaseFamily.Husband.BirthDate})";
 
-        string IDisplayChildrenStatus.Wife
-        {
-            get { return BaseFamily.Wife == null ? string.Empty : BaseFamily.Wife.Name + " (b." + BaseFamily.Wife.BirthDate + ")"; }
-        }
+        string IDisplayChildrenStatus.Wife =>
+                BaseFamily.Wife == null ? string.Empty : $"{BaseFamily.Wife.Name} (b.{BaseFamily.Wife.BirthDate})";
     }
 }
