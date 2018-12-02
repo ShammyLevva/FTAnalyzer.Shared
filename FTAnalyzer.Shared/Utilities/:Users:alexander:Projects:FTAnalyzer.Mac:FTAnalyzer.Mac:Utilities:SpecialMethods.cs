6a013516fa23ce9104f5548688cf0089e4750821
ShammyLevva <ambisset@btinternet.com>
2018-12-02 22:54:01 +0000

    move files

---
+++ FTAnalyzer.Shared/Utilities/:Users:alexander:Projects:FTAnalyzer.Mac:FTAnalyzer.Mac:Utilities:SpecialMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FTAnalyzer.Utilities
+{
+    public static class SpecialMethods
+    {
+
+        public static async Task<TrackingResult> TrackEventAsync(this SimpleTracker tracker, string category, string action, string label, long value = 1)
+        {
+            var eventTrackingParameters = new EventTracking
+            {
+                ClientId = Properties.Settings.Default.GUID.ToString(),
+                UserId = Properties.Settings.Default.GUID.ToString(),
+
+                ApplicationName = "FTAnalyzer",
+                ApplicationVersion = MainForm.VERSION,
+                Category = category,
+                Action = action,
+                Label = label,
+                Value = value,
+                ScreenName = category,
+                ScreenResolution = Screen.PrimaryScreen.Bounds.ToString(),
+                CacheBuster = tracker.AnalyticsSession.GenerateCacheBuster()
+            };
+            return await tracker.TrackAsync(eventTrackingParameters);
+        }
+
+        public static async Task<TrackingResult> TrackScreenviewAsync(this SimpleTracker tracker, string screen)
+        {
+            var screenViewTrackingParameters = new ScreenviewTracking
+            {
+                ClientId = Properties.Settings.Default.GUID.ToString(),
+                UserId = Properties.Settings.Default.GUID.ToString(),
+
+                ApplicationName = "FTAnalyzer",
+                ApplicationVersion = MainForm.VERSION,
+                ScreenName = screen,
+                ScreenResolution = Screen.PrimaryScreen.Bounds.ToString(),
+                CacheBuster = tracker.AnalyticsSession.GenerateCacheBuster()
+            };
+            Console.WriteLine(Properties.Settings.Default.GUID.ToString());
+            return await tracker.TrackAsync(screenViewTrackingParameters);
+        }
+    }
+}
