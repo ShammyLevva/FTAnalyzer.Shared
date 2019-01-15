4e531b8248f91c8a741a120fdf97ece68773cdbc
Alexander Bisset <ambisset@btinternet.com>
2019-01-15 17:36:51 +0000

    Beta 15 - Lost cousins checks existing entries on website

---
+++ FTAnalyzer.Shared/Core/LostCousin.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace FTAnalyzer
+{
+    public class LostCousin : IEquatable<LostCousin>
+    {
+        public string Name { get; }
+        public int BirthYear { get; }
+        public string Reference { get; }
+        public CensusDate CensusDate { get; }
+        string SurnameMetaphone { get; }
+        string ForenamesMetaphone { get; }
+        public LostCousin(string name, int birthYear, string reference, int censusYear, string censusCountry)
+        {
+            Name = name;
+            BirthYear = birthYear;
+            Reference = FixReference(reference);
+            if (censusYear == 1841 && Countries.IsEnglandWales(censusCountry))
+                CensusDate = CensusDate.EWCENSUS1841;
+            if (censusYear == 1881 && Countries.IsEnglandWales(censusCountry))
+                CensusDate = CensusDate.EWCENSUS1881;
+            if (censusYear == 1881 && censusCountry == Countries.SCOTLAND)
+                CensusDate = CensusDate.SCOTCENSUS1881;
+            if (censusYear == 1881 && censusCountry == Countries.CANADA)
+                CensusDate = CensusDate.CANADACENSUS1881;
+            //if(censusYear == 1911 && censusCountry == Countries.IRELAND)
+            //    CensusDate = CensusDate.IRELANDCENSUS1911;
+            if (censusYear == 1911 && Countries.IsEnglandWales(censusCountry))
+                CensusDate = CensusDate.EWCENSUS1911;
+            if (censusYear == 1880 && censusCountry == Countries.UNITED_STATES)
+                CensusDate = CensusDate.USCENSUS1880;
+            if (censusYear ==1940 && censusCountry == Countries.UNITED_STATES)
+                CensusDate = CensusDate.USCENSUS1940;
+
+            int ptr = Name.IndexOf(",", StringComparison.Ordinal);
+            if (ptr > 0)
+            {
+                string forenames = Name.Substring(ptr + 2);
+                string surname = Name.Substring(0, ptr);
+                ForenamesMetaphone = new DoubleMetaphone(forenames).PrimaryKey;
+                SurnameMetaphone = new DoubleMetaphone(surname).PrimaryKey;
+            }
+            else
+            {
+                ForenamesMetaphone = string.Empty;
+                SurnameMetaphone = string.Empty;
+            }
+        }
+
+        public LostCousin(string name, string birthYear, string reference, string census)
+        {
+            Name = name;
+            int.TryParse(birthYear, out int result);
+            BirthYear = result;
+            Reference = reference;
+            if(census.StartsWith("England"))
+            {
+                if (census.EndsWith("1841")) CensusDate = CensusDate.EWCENSUS1841;
+                if (census.EndsWith("1881")) CensusDate = CensusDate.EWCENSUS1881;
+                if (census.EndsWith("1911")) CensusDate = CensusDate.EWCENSUS1911;
+            }
+            if (census.StartsWith("Scotland") && census.EndsWith("1881")) CensusDate = CensusDate.SCOTCENSUS1881;
+            if (census.StartsWith("Canada") && census.EndsWith("1881")) CensusDate = CensusDate.SCOTCENSUS1881;
+            if (census.StartsWith("Ireland") && census.EndsWith("1881")) CensusDate = CensusDate.SCOTCENSUS1881;
+            if (census.StartsWith("United States") && census.EndsWith("1880")) CensusDate = CensusDate.USCENSUS1880;
+            if (census.StartsWith("United States") && census.EndsWith("1940")) CensusDate = CensusDate.USCENSUS1940;
+        }
+
+        string FixReference(string reference)
+        {
+            string output = reference;
+            if (output.Length > 24)
+                output = output.Substring(23); // strip the leading &census_code=XXXX&ref1=
+            output = output.Replace("&ref2=", "/").Replace("&ref3=", "/").Replace("&ref4=", "/").Replace("&ref5=", "/");
+            output = output.Replace("//", "/").Replace("//", "/").TrimEnd('/');
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} b.{BirthYear}, ref: {Reference} {CensusDate}";
+        }
+
+        public bool Equals(LostCousin other)
+        {
+            return CensusDate == other.CensusDate &&
+                    (Name == other.Name || (ForenamesMetaphone == other.ForenamesMetaphone &&
+                                            SurnameMetaphone == other.SurnameMetaphone)) &&
+                    Math.Abs(BirthYear - other.BirthYear) < 2 &&
+                    Reference == other.Reference;
+        }
+    }
+}
--- FTAnalyzer.Shared/Exports/ExportToLostCousins.cs
+++ FTAnalyzer.Shared/Exports/ExportToLostCousins.cs
@@ -1,4 +1,5 @@
 ﻿using FTAnalyzer.Utilities;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,28 +14,40 @@ namespace FTAnalyzer.Exports
         static List<CensusIndividual> ToProcess { get; set; }
         static NetworkCredential Credentials { get; set; }
         static CookieCollection CookieJar { get; set; }
+        static List<LostCousin> OnWebsite { get; set; }
         
         public static int ProcessList(List<CensusIndividual> individuals, IProgress<string> outputText)
         {
             ToProcess = individuals;
             int recordsAdded = 0;
             int recordsFailed = 0;
+            int recordsPresent = 0;
             int count = 0;
+            if(OnWebsite == null)
+                OnWebsite = LoadWebsiteAncestors(outputText);
             foreach (CensusIndividual ind in ToProcess)
             {
-                LostCousinsCensusReference censusRef = ind.CensusReference as LostCousinsCensusReference;
-                if (censusRef.IsValid)
+                if (ind.CensusReference != null && ind.CensusReference.IsValidLostCousinsReference())
                 {
-                    if (AddIndividualToWebsite(ind, outputText))
+                    if (!AlreadyPresent(ind, outputText))
                     {
-                        outputText.Report($"Record {++count} of {ToProcess.Count}: {ind.CensusDate} - {ind.ToString()}, {ind.CensusReference} added.\n");
-                        recordsAdded++;
-                        DatabaseHelper.Instance.StoreLostCousinsFact(ind);
+                        if (AddIndividualToWebsite(ind, outputText))
+                        {
+                            outputText.Report($"Record {++count} of {ToProcess.Count}: {ind.CensusDate} - {ind.ToString()}, {ind.CensusReference} added.\n");
+                            recordsAdded++;
+                            DatabaseHelper.Instance.StoreLostCousinsFact(ind);
+                        }
+                        else
+                        {
+                            outputText.Report($"Record {++count} of {ToProcess.Count}: {ind.CensusDate} - Failed to add {ind.ToString()}, {ind.CensusReference}.\n");
+                            recordsFailed++;
+                        }
                     }
                     else
                     {
-                        outputText.Report($"Record {++count} of {ToProcess.Count}: {ind.CensusDate} - Failed to add {ind.ToString()}, {ind.CensusReference}.\n");
-                        recordsFailed++;
+                        outputText.Report($"Record {++count} of {ToProcess.Count}: {ind.CensusDate} - Already Present {ind.ToString()}, {ind.CensusReference}.\n");
+                        DatabaseHelper.Instance.StoreLostCousinsFact(ind);
+                        recordsPresent++;
                     }
                 }
                 else
@@ -43,7 +56,7 @@ namespace FTAnalyzer.Exports
                     recordsFailed++;
                 }
             }
-            outputText.Report($"\nFinished writing Entries to Lost Cousins website. {recordsAdded} successfully added {recordsFailed} failed. View Lost Cousins Report tab to see current status.");
+            outputText.Report($"\nFinished writing Entries to Lost Cousins website. {recordsAdded} successfully added, {recordsPresent} already present and {recordsFailed} failed.\nView Lost Cousins Report tab to see current status.");
             return recordsAdded;
         }
 
@@ -82,6 +95,44 @@ namespace FTAnalyzer.Exports
             {
                 resp?.Close();
             }
+        }
+
+        static List<LostCousin> LoadWebsiteAncestors(IProgress<string> outputText)
+        {
+            List<LostCousin> websiteList = new List<LostCousin>(); 
+            try
+            {
+                CookieAwareWebClient wc = new CookieAwareWebClient(CookieJar);
+                HtmlDocument doc = new HtmlDocument();
+                string webData = wc.DownloadString("https://www.lostcousins.com/pages/members/ancestors/");
+                doc.LoadHtml(webData);
+                HtmlNodeCollection rows = doc.DocumentNode.SelectNodes("//table[@class='data_table']/tr");
+                foreach(HtmlNode node in rows)
+                {
+                    HtmlNodeCollection columns = node.SelectNodes("td");
+                    if(columns.Count == 8 && columns[0].InnerText != "Name") // ignore header row
+                    { 
+                        string name = columns[0].InnerText.ClearWhiteSpace();
+                        string birthYear = columns[2].InnerText.ClearWhiteSpace();
+                        string reference = columns[4].InnerText.ClearWhiteSpace();
+                        string census = columns[5].InnerText.ClearWhiteSpace();
+                        LostCousin lc = new LostCousin(name, birthYear, reference, census);
+                        websiteList.Add(lc);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                outputText.Report($"Problem accessing Lost Cousins Website to read current ancestor list. Error message is: {e.Message}\n");
+                return null;
+            }
+            return websiteList;
+        }
+
+        static bool AlreadyPresent(CensusIndividual ind, IProgress<string> outputText)
+        {
+            LostCousin lc = new LostCousin($"{ind.Surname}, {ind.Forenames}", ind.BirthDate.BestYear, GetCensusSpecificFields(ind), ind.CensusDate.BestYear, ind.CensusCountry);
+            return OnWebsite.Contains(lc);
         }
 
         static bool AddIndividualToWebsite(CensusIndividual ind, IProgress<string> outputText)
@@ -151,22 +202,22 @@ namespace FTAnalyzer.Exports
 
         static string GetCensusSpecificFields(CensusIndividual ind)
         {
-            LostCousinsCensusReference censusRef = ind.CensusReference as LostCousinsCensusReference;
-            if (ind.CensusDate.Equals(CensusDate.EWCENSUS1841) && Countries.IsEnglandWales(ind.CensusCountry))
+            CensusReference censusRef = ind.CensusReference;
+            if (ind.CensusDate.Overlaps(CensusDate.EWCENSUS1841) && Countries.IsEnglandWales(ind.CensusCountry))
                 return $"&census_code=1841&ref1={censusRef.Piece}&ref2={censusRef.Book}&ref3={censusRef.Folio}&ref4={censusRef.Page}&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.EWCENSUS1881) && Countries.IsEnglandWales(ind.CensusCountry))
+            if (ind.CensusDate.Overlaps(CensusDate.EWCENSUS1881) && Countries.IsEnglandWales(ind.CensusCountry))
                 return $"&census_code=RG11&ref1={censusRef.Piece}&ref2={censusRef.Folio}&ref3={censusRef.Page}&ref4=&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.SCOTCENSUS1881) && ind.CensusCountry == Countries.SCOTLAND)
+            if (ind.CensusDate.Overlaps(CensusDate.SCOTCENSUS1881) && ind.CensusCountry == Countries.SCOTLAND)
                 return $"&census_code=SCT1&ref1={censusRef.RD}&ref2={censusRef.ED}&ref3={censusRef.Page}&ref4=&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.CANADACENSUS1881) && ind.CensusCountry == Countries.CANADA)
+            if (ind.CensusDate.Overlaps(CensusDate.CANADACENSUS1881) && ind.CensusCountry == Countries.CANADA)
                 return $"&census_code=CAN1&ref1={censusRef.ED}&ref2={censusRef.SD}&ref3=&ref4={censusRef.Page}&ref5={censusRef.Family}";
-            //if (ind.CensusDate.Equals(CensusDate.IRELANDCENSUS1911) && ind.CensusCountry == Countries.IRELAND)
+            //if (ind.CensusDate.Overlaps(CensusDate.IRELANDCENSUS1911) && ind.CensusCountry == Countries.IRELAND)
             //    return $"&census_code=0IRL&ref1=&ref2=&ref3=&ref4=&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.EWCENSUS1911) && Countries.IsEnglandWales(ind.CensusCountry))
+            if (ind.CensusDate.Overlaps(CensusDate.EWCENSUS1911) && Countries.IsEnglandWales(ind.CensusCountry))
                 return $"&census_code=0ENG&ref1={censusRef.Piece}&ref2={censusRef.Schedule}&ref3=&ref4=&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.USCENSUS1880) && ind.CensusCountry == Countries.UNITED_STATES)
+            if (ind.CensusDate.Overlaps(CensusDate.USCENSUS1880) && ind.CensusCountry == Countries.UNITED_STATES)
                 return $"&census_code=USA1&ref1={censusRef.Roll}&ref2={censusRef.Page}&ref3=&ref4=&ref5=";
-            if (ind.CensusDate.Equals(CensusDate.USCENSUS1940) && ind.CensusCountry == Countries.UNITED_STATES)
+            if (ind.CensusDate.Overlaps(CensusDate.USCENSUS1940) && ind.CensusCountry == Countries.UNITED_STATES)
                 return $"&census_code=USA4&ref1={censusRef.Roll}&ref2={censusRef.ED}&ref3={censusRef.Page}&ref4=&ref5=";
             return string.Empty;
         }
@@ -189,6 +240,26 @@ namespace FTAnalyzer.Exports
                 default:
                     return "Unknown&descent=";
             }
+        }
+    }
+
+    class CookieAwareWebClient : WebClient
+    {
+        CookieContainer _cookieJar = new CookieContainer();
+
+        internal CookieAwareWebClient(CookieCollection cookies)
+        {
+            _cookieJar.Add(cookies);
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest webRequest)
+            {
+                webRequest.CookieContainer = _cookieJar;
+            }
+            return request;
         }
     }
 }
