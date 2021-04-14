2f3d683c96b39c64c362c66cb8a3b93d13563434
ShammyLevva <mega2019www@gmail.com>
2021-04-14 21:57:41 +0100

    Catch exception on duplicates posting

--- FTAnalyzer.Shared/Core/FamilyTree.cs
+++ FTAnalyzer.Shared/Core/FamilyTree.cs
@@ -3352,6 +3352,10 @@ namespace FTAnalyzer
                 duplicates = null;
                 return null;
             }
+            catch (Exception e)
+            {
+                UIHelpers.ShowMessage($"Duplicate report encountered a problem. Message was: {e.Message}");
+            }
             maximum.Report(MaxDuplicateScore());
             DeserializeNonDuplicates();
             return BuildDuplicateList(value);
@@ -3362,7 +3366,7 @@ namespace FTAnalyzer
             int score = 0;
             foreach (DuplicateIndividual dup in duplicates)
             {
-                if (dup.Score > score)
+                if (dup != null && dup.Score > score)
                     score = dup.Score;
             }
             return score;
