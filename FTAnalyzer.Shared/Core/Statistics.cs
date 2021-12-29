b9a49ee5fbe2f566135f02b06fc275d95d84d530
ShammyLevva <mega2019www@gmail.com>
2021-12-29 20:09:16 +0000

    changed $ strings

--- FTAnalyzer.Shared/Core/Statistics.cs
+++ FTAnalyzer.Shared/Core/Statistics.cs
@@ -67,20 +67,20 @@ namespace FTAnalyzer
             output.AppendLine("Fathers Age When Child Born");
             for (int fiveyear = 3; fiveyear < 20; fiveyear++)
             {
-                string range = (fiveyear * 5) + " to " + (fiveyear * 5 + 4) + " : ";
-                output.AppendLine(range + minAge[0, fiveyear, 0] + " Males, " + minAge[0, fiveyear, 1] + " Females " + minAge[0, fiveyear, 2] + " unknown.");
+                string range = $"{(fiveyear * 5)} to {(fiveyear * 5 + 4)} : ";
+                output.AppendLine($"{range}{minAge[0, fiveyear, 0]} Males, {minAge[0, fiveyear, 1]} Females, {minAge[0, fiveyear, 2]} unknown.");
             }
             output.AppendLine();
             output.AppendLine("Mothers Age When Child Born");
             for (int fiveyear = 3; fiveyear < 13; fiveyear++)
             {
-                string range = (fiveyear * 5) + " to " + (fiveyear * 5 + 4) + " : ";
-                output.AppendLine(range + minAge[1, fiveyear, 0] + " Males, " + minAge[1, fiveyear, 1] + " Females " + minAge[1, fiveyear, 2] + " unknown.");
+                string range = $"{(fiveyear * 5)} to {(fiveyear * 5 + 4)} : ";
+                output.AppendLine($"{range}{minAge[1, fiveyear, 0]} Males, {minAge[1, fiveyear, 1]} Females, {minAge[1, fiveyear, 2]} unknown.");
             }
             int over65males = minAge[1, 13, 0] + minAge[1, 14, 0] + minAge[1, 15, 0] + minAge[1, 16, 0] + minAge[1, 17, 0] + minAge[1, 18, 0] + minAge[1, 19, 0];
             int over65females = minAge[1, 13, 1] + minAge[1, 14, 1] + minAge[1, 15, 1] + minAge[1, 16, 1] + minAge[1, 17, 1] + minAge[1, 18, 1] + minAge[1, 19, 1];
             int over65unknown = minAge[1, 13, 2] + minAge[1, 14, 2] + minAge[1, 15, 2] + minAge[1, 16, 2] + minAge[1, 17, 2] + minAge[1, 18, 2] + minAge[1, 19, 2];
-            output.AppendLine("over 65 : " + over65males + " Males, " + over65females + " Females " + over65unknown + " unknown.");
+            output.AppendLine($"Over 65 : {over65males} Males, {over65females} Females, {over65unknown} unknown.");
             return output.ToString();
         }
 
