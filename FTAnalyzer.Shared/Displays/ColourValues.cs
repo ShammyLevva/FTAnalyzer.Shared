0841582e0c91f10be481da0b4c8550b4dcc07cd0
Karl <karl1c@hotmail.com>
2023-10-21 19:25:41 +0100

    Had to add using System.Drawing to compile cherry pick from master f34af3a.

--- FTAnalyzer.Shared/Displays/ColourValues.cs
+++ FTAnalyzer.Shared/Displays/ColourValues.cs
@@ -1,15 +1,16 @@
-﻿namespace FTAnalyzer
+﻿using System.Drawing;
+namespace FTAnalyzer
 {
     public static class ColourValues
     {
-        [Flags]
+        [System.Flags]
         public enum BMDColours
         {
             EMPTY = 0, UNKNOWN_DATE = 1, OPEN_ENDED_DATE = 13, VERY_WIDE_DATE = 2, WIDE_DATE = 3, JUST_YEAR_DATE = 4, NARROW_DATE = 5, APPROX_DATE = 6,
             EXACT_DATE = 7, NO_SPOUSE = 8, NO_PARTNER = 9, NO_MARRIAGE = 10, ISLIVING = 11, OVER90 = 12, ALL_RECORDS = 99
         };
 
-        [Flags]
+        [System.Flags]
         public enum CensusColours
         {
             NOT_ALIVE = 0, NO_CENSUS = 1, CENSUS_PRESENT_LC_MISSING = 2, CENSUS_PRESENT_NOT_LC_YEAR = 3, CENSUS_PRESENT_LC_PRESENT = 4,
