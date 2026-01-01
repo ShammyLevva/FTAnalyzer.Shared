806bd59c5a69e384706c52854d248637d4862b7f
ShammyLevva <ambisset@btinternet.com>
2026-01-01 13:53:57 +0000

    Add registry settings helper class

---
+++ FTAnalyzer.Shared/Utilities/RegistrySettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+
+namespace FTAnalyzer.Shared.Utilities
+{
+    public static class RegistrySettings
+    {
+        static string VersionIndependentRegKey
+        {
+            get
+            {
+                string versionDependent = Application.UserAppDataRegistry.Name;
+                string versionIndependent =
+                       versionDependent.Substring(0, versionDependent.LastIndexOf("\\"));
+                return versionIndependent;
+            }
+        }
+        public static object? GetValue(string name, object defaultValue)
+        {
+            return Registry.GetValue(VersionIndependentRegKey, name, defaultValue);
+        }
+        public static object? GetRegistryValue(string name)
+        {
+            return GetValue(name, null);
+        }
+        public static void SetValue(string name, object value, RegistryValueKind kind)
+        {
+            Registry.SetValue(VersionIndependentRegKey, name, value, kind);
+        }
+    }
+}
