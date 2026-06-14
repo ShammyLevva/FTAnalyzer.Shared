94805a6b29f27c7faf38885ed273c5289a8ce73c
ShammyLevva <ambisset@btinternet.com>
2026-06-14 22:14:48 +0100

    force creation of registry key if it doesnt exist before

--- FTAnalyzer.Shared/Utilities/RegistrySettings.cs
+++ FTAnalyzer.Shared/Utilities/RegistrySettings.cs
@@ -11,14 +11,8 @@ namespace FTAnalyzer.Shared.Utilities
         {
             get
             {
-                RegistryKey? softKey = Registry.CurrentUser.OpenSubKey("Software");
-                RegistryKey? companyKey = softKey?.OpenSubKey(APPNAME);
-                if (companyKey is null)
-                    softKey.CreateSubKey(APPNAME);
-                RegistryKey? appKey = companyKey?.OpenSubKey(APPNAME);
-                if (appKey is null)
-                    companyKey.CreateSubKey(APPNAME);
-                return appKey.ToString();
+                using RegistryKey appKey = Registry.CurrentUser.CreateSubKey($@"Software\{APPNAME}\{APPNAME}", true);
+                return appKey.Name;
             }
         }
         public static int GetIntRegistryValue(string name, int defaultValue)
